a6b7e4adf93a38d9ba002dab869f4cd8b799b6db
brenopmir <143268744+brenopmir@users.noreply.github.com>
2023-10-29 20:47:07 -0300

    Update tranca.cc

--- src/tranca.cc
+++ src/tranca.cc
@@ -1,52 +1,25 @@
-#include "../include/Lampada.h"
+#include "../include/tranca.h"
 
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-Lampada::Lampada(){   
+Tranca::Tranca(){   
 }
 
-void Lampada::SetIntensidade(int i){
-    if(i < 0 || i > 100){
-        throw Intensidade_invalida{i};
-    }
-    else{
-       intensidade = i;
-    }
+void Tranca::SetNome(string name){
+  nome=name;
 }
 
-int Lampada::Intensidade(){
-    return intensidade;
+string Tranca::Nome(){
+  return nome;
 }
 
-void Lampada::SetCor(string c){
-    bool valido = false;
-    for(auto it = cores.begin(); it != cores.end(); it++){
-        if(c == *it){
-            c == *it;
-            valido = true;
-        }
-    }
-    if(!(valido))
-       throw Cor_Invalida{c};
+void Tranca::SetAtiva(bool at){
+  ativa=at;    
 }
 
-string Lampada::Cor(){
-    return cor;
-}
-
-void Lampada::ListarCores(){
-  for(auto it = cores.begin(); it != cores.end(); it++){
-    std::cout << *it << endl;
-}
-}
-
-void Lampada::SetNome(string name){
-  nome = name;
-}
-
-string Lampada::Nome(){
-    return nome;
+bool Tranca::Ativa(){
+  return ativa;
 }
