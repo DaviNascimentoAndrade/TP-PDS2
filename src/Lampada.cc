a8393c00324a035171af525974c4fa13b0fbbf23
DaviNascimentoAndrade <114942566+DaviNascimentoAndrade@users.noreply.github.com>
2023-10-31 17:21:02 -0300

    Update Lampada.cc
    Coloquei os _ nas variáveis private pra funcionar com o .h

--- src/Lampada.cc
+++ src/Lampada.cc
@@ -13,19 +13,19 @@ void Lampada::SetIntensidade(int i){
     throw Intensidade_invalida{i};
     }
   else{
-    intensidade = i;
+    intensidade_ = i;
     }
 }
 
 int Lampada::Intensidade(){
-  return intensidade;
+  return intensidade_;
 }
 
 void Lampada::SetCor(string c){
   bool valido = false;
-  for(auto it = cores.begin(); it != cores.end(); it++){
+  for(auto it = cores_.begin(); it != cores_.end(); it++){
     if(c == *it){
-      cor = *it;
+      cor_ = *it;
       valido = true;
     }
   }
@@ -35,17 +35,17 @@ void Lampada::SetCor(string c){
 }
 
 string Lampada::Cor() {
-  return cor;
+  return cor_;
 }
 
 void Lampada::ListarCores(){
-  for(auto it = cores.begin(); it != cores.end(); it++){
+  for(auto it = cores_.begin(); it != cores_.end(); it++){
     std::cout << *it << endl;
 }
 }
 
 void Lampada::SetNome(string name){
-  nome = name;
+  nome_ = name;
 }
 
 string Lampada::Nome(){
