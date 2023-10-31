e693c6f1361385e12ef04d499e1f39e7d9dda7bb
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-10-30 21:56:05 -0300

    padronização lampada.h

--- include/Lampada.h
+++ include/Lampada.h
@@ -16,32 +16,32 @@ string cor;
 
 class Lampada{
 public:
-    
-    // Cria uma lâmpada sem nome com intensidade 0;
-     Lampada();
 
-    //Configura a intensidade da lâmpada;
-    //PRECONDIÇÃO: A intensidade deve ser de 0 a 100;
-    void SetIntensidade(int i);
+// Cria uma lâmpada sem nome com intensidade 0;
+Lampada();
 
-    //Retorna intensidade da lâmpada;
-    int Intensidade ();
+//Configura a intensidade da lâmpada;
+//PRECONDIÇÃO: A intensidade deve ser de 0 a 100;
+void SetIntensidade(int i);
 
-    //PRECONDIÇÃO: Devem ser inseridas somente cores válidas, como amarelo,vermelho, azul,branco,laranja,verde ou roxo
-    //Seta a cor da lãmpada
-    void SetCor(string c);
+//Retorna intensidade da lâmpada;
+int Intensidade ();
 
-    //Retorna a cor atual da lâmpada;
-    string Cor();
+//PRECONDIÇÃO: Devem ser inseridas somente cores válidas, como amarelo,vermelho, azul,branco,laranja,verde ou roxo
+//Seta a cor da lãmpada
+void SetCor(string c);
 
-    //Lista as cores que a lâmpada pode mostrar;
-    void ListarCores();
+//Retorna a cor atual da lâmpada;
+string Cor();
 
-    //Muda o nome do dispositivo;
-    void SetNome(string name);
+//Lista as cores que a lâmpada pode mostrar;
+void ListarCores();
 
-    //Retorna o nome do dispositivo.
-    string Nome();
+//Muda o nome do dispositivo;
+void SetNome(string name);
+
+//Retorna o nome do dispositivo.
+string Nome();
 
 
 private:
