9d31ae171b1767aac069115754ddaf822fc68fe7
LucasVellosoCPE <148832363+LucasVellosoCPE@users.noreply.github.com>
2023-10-31 20:59:14 -0300

    Create testeLampada.cc

---
+++ testeLampada.cc
@@ -0,0 +1,18 @@
+#include "lampada.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nome = "joaozinho";
+    string cor = "Vermelho";
+    Lampada teste;
+    teste.SetIntensidade(85);
+    teste.SetCor(cor);
+    teste.ListarCores();
+    teste.SetNome(nome);
+    cout << teste.Nome() <<endl;
+    cout << teste.Intensidade() << endl;
+    cout << teste.Cor() << endl;
+
+}
