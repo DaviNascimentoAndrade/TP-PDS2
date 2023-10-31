26e588986c6ac30b88ab0300061e5d41811c9a27
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-10-30 21:55:35 -0300

    padronizacao cortina.h

--- include/Cortina.h
+++ include/Cortina.h
@@ -6,25 +6,25 @@
 using namespace std;
 class Cortina {
 public:
-    // Construtor da classe Cortina
-    Cortina(); // Construtor
+// Construtor da classe Cortina
+Cortina(); // Construtor
 
-    // Configura a quantidade que a cortina está fechada ou aberta.
-    // @param novaIntensidade: A nova intensidade da cortina (0-100).
-    void SetIntensidade(int novaIntensidade);
+// Configura a quantidade que a cortina está fechada ou aberta.
+// @param novaIntensidade: A nova intensidade da cortina (0-100).
+void SetIntensidade(int novaIntensidade);
 
-    // Retorna o status de iluminação da cortina.
-    int Intensidade();
+// Retorna o status de iluminação da cortina.
+int Intensidade();
 
-    // Retorna o nome da cortina.
-    string Nome(); 
+// Retorna o nome da cortina.
+string Nome(); 
 
-    // Muda o nome do dispositivo.
-    void SetNome(string novoNome);
+// Muda o nome do dispositivo.
+void SetNome(string novoNome);
 
 private:
-    int intensidade_cortina;
-    string nome; 
+int intensidade_cortina;
+string nome; 
 };
 
 #endif
