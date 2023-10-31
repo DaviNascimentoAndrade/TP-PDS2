ea0d99f2d49d845251fb036b76518c782855a7d5
brenopmir <143268744+brenopmir@users.noreply.github.com>
2023-10-30 22:59:14 -0300

    Update cortina.cc

--- src/cortina.cc
+++ src/cortina.cc
@@ -1,25 +1,26 @@
-#include "cortina.h"
+#include "../include/cortina.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
-    Cortina::Cortina(){
-        intensidade_cortina = 0;
+  Cortina::Cortina(){
+      intensidade_cortina = 0;
     }
     void Cortina::SetIntensidade(int novaIntensidade) {
-        if (novaIntensidade >= 0 && novaIntensidade <= 100) {
-            intensidade_cortina = novaIntensidade;
-        } else {
-            cout << "A intensidade deve estar entre 0 e 100." <<endl;
+      if (novaIntensidade >= 0 && novaIntensidade <= 100) {
+          intensidade_cortina = novaIntensidade;
+        } 
+      else {
+          cout << "A intensidade deve estar entre 0 e 100." <<endl;
         }
     }
     int Cortina::Intensidade() {
-        return intensidade_cortina;
+      return intensidade_cortina;
     }
     string Cortina::Nome(){
-        return nome;
+      return nome;
     }
     void Cortina::SetNome(string novoNome){
-        nome = novoNome;
+      nome = novoNome;
     } 
     
