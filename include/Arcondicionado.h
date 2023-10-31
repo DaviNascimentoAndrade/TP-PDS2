1776e00b5c8c5ce5d8330ac610b9cf96646bcf35
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-10-30 21:54:44 -0300

    padronização .h

--- include/Arcondicionado.h
+++ include/Arcondicionado.h
@@ -10,41 +10,41 @@ class ArCondicionado {
 
     public:
 
-        // cria um novo arcondicionado sem nome
-        ArCondicionado();
+// cria um novo arcondicionado sem nome
+ArCondicionado();
 
-        // coloca um nome para o ar condicionado
-        void SetNome(string nome);
+// coloca um nome para o ar condicionado
+void SetNome(string nome);
 
-        // retorna o nome do ar condicionado
-        string Nome();
+// retorna o nome do ar condicionado
+string Nome();
 
-        // muda o modo do ar condicionado
-        //PRECONDIÇÃO: Deve ser cool, heat, dry ou fan
-        void SetModo(string modo);
+// muda o modo do ar condicionado
+//PRECONDIÇÃO: Deve ser cool, heat, dry ou fan
+void SetModo(string modo);
 
-        // retorna o modo do ar condicionado
-        string Modo();
+// retorna o modo do ar condicionado
+string Modo();
 
-        // liga ou desliga o ar condicionado(0 desligado)
-        void SetLigar(bool li);
+// liga ou desliga o ar condicionado(0 desligado)
+void SetLigar(bool li);
 
-        // retorna status da tranca da janela
-        bool Ligado();
+// retorna status da tranca da janela
+bool Ligado();
 
-        // configura temperatura do ar-condicionado.
-        //PRECONDIÇÃO:a temperatura deve ter valor entre 16 e 30
-        void SetTemperatura(int temp); 
+// configura temperatura do ar-condicionado.
+//PRECONDIÇÃO:a temperatura deve ter valor entre 16 e 30
+void SetTemperatura(int temp); 
         
-        //retorna temperatura do ar condicionado
-        int Temperatura ();
+//retorna temperatura do ar condicionado
+int Temperatura ();
 
 
-    private:
-        int temperatura;
-        bool ligado;
-        std::string modo;
-        std::string nome;
+private:
+int temperatura;
+bool ligado;
+std::string modo;
+std::string nome;
 };
 
 #endif ARCONDICIONADO_H_
