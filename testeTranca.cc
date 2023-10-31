8928ad16dc5cd76582bae3fbde7e2034d38f780b
LucasVellosoCPE <148832363+LucasVellosoCPE@users.noreply.github.com>
2023-10-31 20:58:35 -0300

    Create testeTranca.cc

---
+++ testeTranca.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "include/tranca.h"
+
+//Pra testar a tranca
+
+using namespace std;
+
+int main(){
+    Tranca breno;
+    breno.SetNome("breno");
+    cout << breno.Nome() << endl;
+    breno.SetAtiva(true);
+    cout << breno.Ativa() << endl;
+
+}
