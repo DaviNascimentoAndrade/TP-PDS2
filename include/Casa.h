da888b14a5627f2e08f28cd21b17d684a3fe30bd
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-10-30 21:55:09 -0300

    padronização casa.h

--- include/Casa.h
+++ include/Casa.h
@@ -9,41 +9,41 @@ using namespace std;
 
 class Casa {
 
-    public:
+public:
 
-        // cria uma novo casasem nome
-        Casa();
+// cria uma novo casasem nome
+Casa();
 
-        // coloca um nome para a casa
-        void SetNome(string nm);
+// coloca um nome para a casa
+void SetNome(string nm);
 
-        // retorna o nome da casa
-        string Nome();
+// retorna o nome da casa
+string Nome();
         
-        //Lista todos os cômodos da casa;
-        void ListarComodos(); 
+//Lista todos os cômodos da casa;
+void ListarComodos(); 
         
-        //Enquanto essa função estiver ativa, o usuário pode configurar todos os dispositivos da casa normalmente e todas as configurações feitas serão salvas em um modo;
-        void ConfigurarModo(string nm); 
+//Enquanto essa função estiver ativa, o usuário pode configurar todos os dispositivos da casa normalmente e todas as configurações feitas serão salvas em um modo;
+void ConfigurarModo(string nm); 
         
-        //Aplica as conficurações de um modo salvo;
-        void AtivarModo (string nm);  
+//Aplica as conficurações de um modo salvo;
+void AtivarModo (string nm);  
         
-        //Lista os modos da casa;
-        void ListarModos(); 
+//Lista os modos da casa;
+void ListarModos(); 
         
-        //Adiciona um cômodo para que o usuário possa edita-lo;
-        void AdicionarComodo(string nm); 
+//Adiciona um cômodo para que o usuário possa edita-lo;
+void AdicionarComodo(string nm); 
        
-       //Apaga um cômodo e todos os dispositivos nele; 
-        void RemoverComodo(string nm);
+//Apaga um cômodo e todos os dispositivos nele; 
+void RemoverComodo(string nm);
 
 
-    private:
-        class Comodo;
-        map<string, Comodo> comodos;
-        map<string, Casa> modos;
-        string nome;
+private:
+class Comodo;
+map<string, Comodo> comodos;
+map<string, Casa> modos;
+string nome;
 
 };
 
