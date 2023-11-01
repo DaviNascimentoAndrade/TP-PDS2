a103d5880ae950ca7c473e862702d0993afeae7b
art-caetano <148159860+art-caetano@users.noreply.github.com>
2023-10-31 23:04:50 -0300

    Create testeArcondicionado.cc

---
+++ testeArcondicionado.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "arcondicionado.h"
+
+using namespace std;
+
+int main(){
+  ArCondicionado teste;
+  teste.SetNome("joao");
+  cout << "O nome do aparelho é " << teste.Nome() << endl;
+  teste.SetModo("cool");
+  cout << "O modo do aparelho é " << teste.Modo() << endl;
+  teste.SetLigar(true);
+  cout << "O estado do aparelho é: " << teste.Ligado() << endl;
+  teste.SetTemperatura(22);
+  cout << "A temperatura do aparelho é " << teste.Temperatura() << endl;
+
+}
