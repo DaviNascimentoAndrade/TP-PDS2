1ce7288a1d14681a50733904880234bd7b182085
DaviNascimetoAndrade <daviandrone3779@gmail.com>
2023-11-24 23:20:36 -0300

    Mais coisa no main: continuar menu de dispositivos

--- src/main.cc
+++ src/main.cc
@@ -11,11 +11,7 @@ Menu principal:
     Voltar
 
     Comodo específico:
-      Ares condiciondados
-      Cortinas
-      Janelas
-      Lâmpadas
-      Trancas
+      Acessar dispositivos()
       Mudar nome()
       Voltar
 
@@ -58,14 +54,16 @@ void MenuComodos(Casa &house); // Configura uma casa
 // MENU CÔMODOS
 void AdicionarComodo(Casa& house, string nome); // Adiciona um cômodo na casa
 void RemoverComodo(Casa& house, string nome); // Remove um comodo da casa
-void MenuComodo(string comodo); // configura um comodo
+void MenuComodo(Casa& house, string comodo); // configura um comodo
 
 // MENU COMODO
-void MudarNome(string& atual); // Muda o nome do cômodo informado
+void MenuDispositivo(Casa& house, string comodo);
 
 // MODOS
 void AdicionarModo(Casa& house, string nome); // Adiciona um modo na casa
 void RemoverModo(Casa& house, string nome); // Remove um modo da casa
+
+void Titulo(string titulo);
 
 /*
 =============================================================================
@@ -75,8 +73,35 @@ MAIN
 
 int main(){
 
+  Casa casa;
 
+  // MENU PRINCIPAL
+  int esc = 0;
+  while (esc != 3){
 
+    system("clear");
+    Titulo("MENU PRINCIPAL");
+    cout<<"  1 - Menu de comodos\n";
+    cout<<"  2 - Menu de modos\n";
+    cout<<"  3 - Sair\n";
+
+    esc = 0;
+    while(esc < 1 || esc > 3){
+      cout<<"Digite sua escolha: ";
+      cin>>esc;
+      if(esc < 1 || esc > 3){cout<<"Escolha invalida!"<<endl;}
+    }
+
+    // MENU DE COMODOS
+    if(esc == 1){
+      MenuComodos(casa);
+    }
+
+    // MENU DE MODOS
+    else if(esc == 2){
+
+    }
+  }
   return 0;
 }
 
@@ -86,6 +111,7 @@ FUNÇÕES
 =============================================================================
 */
 
+// TITULO
 void Titulo (string titulo) {
     cout<<"==============================================================\n";
     cout<<"\t"<< titulo <<"\n";
@@ -93,11 +119,13 @@ void Titulo (string titulo) {
     cout<<"\n";
 }
 
+// MENU COMODOS
 void MenuComodos(Casa &house){
 
   int esc = 0;
   while(esc != 4){
 
+    system("clear");
     Titulo("MENU DE COMODOS");
     cout<<"  1 - Acessar comodo\n";
     cout<<"  2 - Adicionar comodo\n";
@@ -108,31 +136,114 @@ void MenuComodos(Casa &house){
     while(esc < 1 || esc > 4){
       cout<<"Digite sua escolha: ";
       cin>>esc;
-      while(esc < 1 || esc > 4){cout<<"Escolha invalida!";}
+      if(esc < 1 || esc > 4){cout<<"Escolha invalida!"<<endl;}
     }
 
     // ACESSAR COMODO
     if(esc == 1){
 
-      cout<<"Comodos disponiveis:";
-      house.ListarComodos();
-      string comodo = "";
-      bool ex = 0;
-      while(!ex){
+      if(house.comodos_.size() < 1){
+        cout<<"Nao existem comodos, adicione um comodo para usar essta opcao!"<<endl;
+        system("pause");
+      }else{
+        // LISTAR COMODOS DISPONÍVEIS
+        cout<<"Comodos disponiveis:";
+        house.ListarComodos();
+        string comodo = "";
+
+        // USUÁRIO ESCOLHE O CÔMODO QUE QUER CONFIGURAR
+        bool ex = 0;
+        while(!ex){
 
         cout<<"Digite o nome do comodo que deseja acessar: ";
         cin>>comodo;
         bool ex = 0;
       
-        for(auto it : house.comodos_){
-          if(it.first == comodo){
-            ex = 1;
+          for(auto it : house.comodos_){
+            if(it.first == comodo){
+             ex = 1;
+            }
           }
-          
+          if(!ex){cout<<"Escolha invalida!";}
         }
+      
+        MenuComodo(house, comodo);
       }
+    }
+
+    // ADICIONAR COMODO
+    else if(esc == 2){
 
     }
 
+    // REMOVER COMODO
+    else if(esc == 3){
+
+    }
+  }
+}
+
+// MENU COMODO
+void MenuComodo(Casa& house, string comodo){
+
+  int esc = 0;
+  while(esc != 3){
+
+    system("clear");
+    Titulo("DISPOSITIVOS DE "+ comodo);
+    cout<<"  1 - Configurar dispositivo especifico\n";
+    cout<<"  2 - Configurar todos de um tipo\n";
+    cout<<"  3 - Voltar\n";
+    cout<<"  4 - Voltar\n";
+    cout<<"  5 - Voltar\n";
+
+    esc = 0;
+    while(esc < 1 || esc > 3){
+      cout<<"Digite sua escolha: ";
+      cin>>esc;
+      if(esc < 1 || esc > 3){cout<<"Escolha invalida!"<<endl;}
+    }
+  
+
+    // Acessar dispositivos
+    if(esc == 1){
+      MenuDispositivo(house, comodo);
+    }
+
+    // Mudar nome
+    else if(esc == 2){
+
+    }
+  }
+}
+
+// Menu dispositivos
+void MenuDispositivo(Casa& house, string comodo){
+
+  int esc = 0;
+  while(esc != 3){
+
+    system("clear");
+    Titulo("COMODO "+ comodo);
+    cout<<"  1 - Configurar dispositivo\n";
+    cout<<"  2 - Adicionar dispositivo\n";
+    cout<<"  3 - Voltar\n";
+
+    esc = 0;
+    while(esc < 1 || esc > 3){
+      cout<<"Digite sua escolha: ";
+      cin>>esc;
+      if(esc < 1 || esc > 3){cout<<"Escolha invalida!"<<endl;}
+    }
+
+    // Acessar dispositivos
+    if(esc == 1){
+      
+    }
+
+    // Mudar nome
+    else if(esc == 2){
+
+    }
   }
 }
